519696195a873d28527e838a044d31e941c0ad9a
Jędrzej  Choniawko <jchoniawko@gmail.com>
2021-12-30 03:32:57 +0100

    test suites

---
+++ Test Suites/TS_testingapp.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS_testingapp</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>cbf3d75b-fdc5-4377-85c4-b62b7b5c160d</testSuiteGuid>
+</TestSuiteEntity>
